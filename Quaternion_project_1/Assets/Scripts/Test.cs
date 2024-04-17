4fd2c8aa05e6e028211d2c525802f7d53337250f
Nayoun00 <nayounseok12@gmail.com>
2024-04-17 16:17:15 +0900

    feat : for 문과 while 문 작성까지
    이론 위주의 강의

---
+++ Quaternion_project_1/Assets/Scripts/Test.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*for (; ; )
+        {
+
+        }*/
+
+        /*bool whileRunner = false;       //시작부터 false라 실행 안됌
+        while (true)
+        {
+            if(whileRunner == false)
+            {
+                whileRunner = true;
+            }
+            else
+            {
+                whileRunner = false;
+            }
+            Debug.Log("for 문 while 문 ");
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
